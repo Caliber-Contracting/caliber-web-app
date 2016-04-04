bcc49799097136ed7f0c35c2c9d549fd6efab71f
cjurden <colejurden@gmail.com>
2016-04-04 13:47:19 -0500

    cleanup

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(require('connect-assets')());
 
 // Redirect empty path to root URL. Routing is handled internally via lib>router.js.coffee
